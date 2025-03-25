74f2cb225c9ca68d7536c6f831a7e3f593052a12
Orestas Petkus <1764216@activedir.vu.lt>
2025-03-25 15:13:39 +0200

    pirmas

---
+++ pirma.php
@@ -0,0 +1,22 @@
+<?php
+    function collatz_fn($number)
+    {
+        $n = $number;
+
+        while ($n != 1)
+        {
+            if ($n % 2 == 0)
+            {
+                $n /= 2;
+            }
+            else
+            {
+                $n = 3 * $n + 1;
+            }
+        }
+
+        print("baigesi su " . $n);
+    }
+
+    collatz_fn(30);
+?>
